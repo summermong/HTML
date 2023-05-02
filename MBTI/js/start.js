04cbe0576a6a4df64855139e8a6172c21cdb92c2
summermong <2514o_o@naver.com>
2023-05-02 22:18:07 +0900

    MBTI clone v.2 Share function

--- MBTI/js/start.js
+++ MBTI/js/start.js
@@ -1,49 +1,31 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
-const startbtn = document.querySelector("#main button");
 const result = document.querySelector("#result");
-const endPoint = 12;
-const select = [];
+
+const endPoint = 11;
+const select = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 function calResult() {
-  var pointArray = [
-    { name: "mouse", value: 0, key: 0 },
-    { name: "cow", value: 0, key: 1 },
-    { name: "tiger", value: 0, key: 2 },
-    { name: "rabbit", value: 0, key: 3 },
-    { name: "dragon", value: 0, key: 4 },
-    { name: "snake", value: 0, key: 5 },
-    { name: "horse", value: 0, key: 6 },
-    { name: "sheep", value: 0, key: 7 },
-    { name: "monkey", value: 0, key: 8 },
-    { name: "chick", value: 0, key: 9 },
-    { name: "dog", value: 0, key: 10 },
-    { name: "pig", value: 0, key: 11 },
-  ];
+  console.log(select);
+  var result = select.indexOf(Math.max(...select));
+  return result;
+}
 
-  for (let i = 0; i < endPoint; i++) {
-    var target = qnaList[i].a[select[i]];
-    for (let j = 0; j < target.type.length; j++) {
-      for (let k = 0; k < pointArray.length; k++) {
-        if (target.type[j] === pointArray[k].name) {
-          pointArray[k].value += 1;
-        }
-      }
-    }
-  }
+function setResult() {
+  let point = calResult();
+  const resultName = document.querySelector(".resultname");
+  resultName.innerHTML = infoList[point].name;
 
-  var resultArray = pointArray.sort(function (a, b) {
-    if (a.value > b.value) {
-      return -1;
-    }
-    if (a.value < b.value) {
-      return 1;
-    }
-    return 0;
-  });
+  var resultImg = document.createElement("img");
+  const imgDiv = document.querySelector("#resultImg");
+  var imgURL = "img/image-" + point + ".png";
+  resultImg.src = imgURL;
+  resultImg.alt = point;
+  resultImg.classList.add("img-fluid");
+  imgDiv.appendChild(resultImg);
 
-  let resultWord = resultArray[0].key;
-  return resultWord;
+  const resultDesc = document.querySelector(".resultDesc");
+  resultDesc.innerHTML = infoList[point].desc;
 }
 
 function goResult() {
@@ -57,17 +39,17 @@ function goResult() {
       result.style.display = "block";
     }, 450);
   });
-
-  calResult();
+  setResult();
 }
 
 function addAnswer(answerText, qIdx, idx) {
-  let a = document.querySelector(".answerBox");
-  let answer = document.createElement("button");
+  var a = document.querySelector(".answerBox");
+  var answer = document.createElement("button");
   answer.classList.add("answerList");
   answer.classList.add("my-3");
   answer.classList.add("py-3");
   answer.classList.add("mx-auto");
+  answer.classList.add("fadeIn");
 
   a.appendChild(answer);
   answer.innerHTML = answerText;
@@ -75,14 +57,18 @@ function addAnswer(answerText, qIdx, idx) {
   answer.addEventListener(
     "click",
     function () {
-      let children = document.querySelectorAll(".answerList");
+      var children = document.querySelectorAll(".answerList");
       for (let i = 0; i < children.length; i++) {
-        children[i].disable = true;
+        children[i].disabled = true;
         children[i].style.WebkitAnimation = "fadeOut 0.5s";
         children[i].style.animation = "fadeOut 0.5s";
       }
       setTimeout(() => {
-        select[qIdx] = idx;
+        var target = qnaList[qIdx].a[idx].type;
+        for (let i = 0; i < target.length; i++) {
+          select[target[i]] += 1;
+        }
+
         for (let i = 0; i < children.length; i++) {
           children[i].style.display = "none";
         }
@@ -99,13 +85,11 @@ function goNext(qIdx) {
     return;
   }
 
-  let q = document.querySelector(".qBox");
+  var q = document.querySelector(".qBox");
   q.innerHTML = qnaList[qIdx].q;
-
   for (let i in qnaList[qIdx].a) {
     addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
   }
-
   var status = document.querySelector(".statusBar");
   status.style.width = (100 / endPoint) * (qIdx + 1) + "%";
 }
@@ -113,18 +97,14 @@ function goNext(qIdx) {
 function begin() {
   main.style.WebkitAnimation = "fadeOut 1s";
   main.style.animation = "fadeOut 1s";
-
   setTimeout(() => {
     qna.style.WebkitAnimation = "fadeIn 1s";
     qna.style.animation = "fadeIn 1s";
-
     setTimeout(() => {
       main.style.display = "none";
       qna.style.display = "block";
     }, 450);
-    let qIdx = 0; //goNext함수가 처음 실행될 때  인덱스 0
+    let qIdx = 0;
     goNext(qIdx);
   }, 450);
 }
-
-startbtn.addEventListener("click", begin);
