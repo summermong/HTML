f6a9a1e309c819ac0dada847de8e068a2b88afb8
summermong <2514o_o@naver.com>
2023-05-25 00:20:40 +0900

    React Drag and Drop

--- goorm_React/react-todo/src/App.js
+++ goorm_React/react-todo/src/App.js
@@ -1,108 +1,37 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
+import List from "./components/List";
+import Form from "./components/Form";
 
-export default class App extends Component {
-  state = {
-    todoData: [],
-    value: "",
-  };
+export default function App() {
+  const [todoData, setTodoDate] = useState([]);
+  const [value, setValue] = useState("");
 
-  btnStyle = {
-    color: "#fff",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-    float: "right",
-  };
-
-  getStyle = (completed) => {
-    return {
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: completed ? "line-through" : "none",
-    };
-  };
-
-  handleClick = (id) => {
-    let newTodoData = this.state.todoData.filter((data) => data.id !== id);
-    this.setState({ todoData: newTodoData });
-  };
-
-  handleChange = (event) => {
-    this.setState({ value: event.target.value });
-  };
-
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
 
     //새로운 할 일 데이터
     let newTodo = {
       id: Date.now(),
-      title: this.state.value,
+      title: value,
       completed: false,
     };
 
     //새로운 할 일 더해주기
 
-    this.setState({ todoData: [...this.state.todoData, newTodo], value: "" });
+    setTodoDate((prev) => [...prev, newTodo]);
+    setValue("");
   };
 
-  handleCompleteChange = (id) => {
-    let newTodoData = this.state.todoData.map((data) => {
-      if (data.id === id) {
-        data.completed = !data.completed;
-      }
-      return data;
-    });
-
-    this.setState({ todoData: newTodoData });
-  };
-
-  render() {
-    return (
-      <div className="container">
-        <div className="todo-block">
-          <div className="title">
-            <h1> 할 일 목록</h1>
-          </div>
-
-          {this.state.todoData.map((data) => (
-            <div style={this.getStyle(data.completed)} key={data.id}>
-              <input
-                type="checkbox"
-                defaultChecked={false}
-                onChange={() => this.handleCompleteChange(data.id)}
-              />
-              {data.title}
-              <button
-                style={this.btnStyle}
-                onClick={() => this.handleClick(data.id)}
-              >
-                {" "}
-                ❌{" "}
-              </button>
-            </div>
-          ))}
-
-          <form style={{ display: "flex" }} onSubmit={this.handleSubmit}>
-            <input
-              type="text"
-              name="value"
-              style={{ flex: "10", padding: "5px" }}
-              placeholder="해야 할 일을 입력하세요."
-              value={this.state.value}
-              onChange={this.handleChange}
-            ></input>
-            <input
-              type="submit"
-              value="입력"
-              className="btn"
-              style={{ flex: "1" }}
-            ></input>
-          </form>
+  return (
+    <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
+      <div className="w-full p-6 m-4 bg-white rounded shadow-sm lg:w-3/4 lg:max-w-lg">
+        <div className="flex justify-between mb-3">
+          <h1> 할 일 목록</h1>
         </div>
+        <List todoData={todoData} setTodoDate={setTodoDate} />
+        <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
       </div>
-    );
-  }
+    </div>
+  );
 }
---
+++ goorm_React/react-todo/src/components/Form.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Form({ handleSubmit, value, setValue }) {
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <form className="flex pt-2" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="value"
+        className="w-full px-3 py-2 mr-4 text-gray-500 border rounded shadow"
+        placeholder="해야 할 일을 입력하세요."
+        value={value}
+        onChange={handleChange}
+      ></input>
+      <input
+        className="p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-200"
+        type="submit"
+        value="입력"
+      ></input>
+    </form>
+  );
+}
