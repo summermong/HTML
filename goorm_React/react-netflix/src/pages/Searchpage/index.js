9b0e7ac5a188b59aee79a9eaf574c8774db51659
summermong <2514o_o@naver.com>
2023-05-31 18:31:08 +0900

    React no.61 useDebounce Custom Hooks

--- goorm_React/react-netflix/src/pages/Searchpage/index.js
+++ goorm_React/react-netflix/src/pages/Searchpage/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "../../api/axios";
+import "./SearchPage.css";
+import { useDebounce } from "../../hooks/useDebounce";
 
 export default function SearchPage() {
   const [searchResults, setSearchResults] = useState([]);
@@ -10,24 +12,52 @@ export default function SearchPage() {
 
   let query = useQuery();
   const searchTerm = query.get("q");
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
   useEffect(() => {
-    if (searchTerm) {
-      fetchSearchMovie(searchTerm);
+    if (debouncedSearchTerm) {
+      fetchSearchMovie(debouncedSearchTerm);
     }
-  }, [searchTerm]); //의존 배열에 넣은 이유는 검색어를 입력하는 동안에도 해당되는 것들을 보여주게 하기 위함
+  }, [debouncedSearchTerm]); //의존 배열에 넣은 이유는 검색어를 입력하는 동안에도 해당되는 것들을 보여주게 하기 위함
 
   const fetchSearchMovie = async (searchTerm) => {
     try {
       const request = await axios.get(
         `/search/multi?include_adult=false&query=${searchTerm}`
       );
-      console.log(request);
       setSearchResults(request.data.results);
-    } catch (error) {
-      console.log("error", error);
-    }
+    } catch (error) {}
   };
 
-  return <div></div>;
+  const renderSearchResults = () => {
+    return searchResults.length > 0 ? (
+      <section className="search-container">
+        {searchResults.map((movie) => {
+          if (movie.backdrop_path !== null && movie.media_type !== "person") {
+            const movieImageUrl =
+              "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+            return (
+              <div className="movie" key={movie.id}>
+                <div className="movie__column-poster">
+                  <img
+                    className="movie__poster"
+                    src={movieImageUrl}
+                    alt="movie"
+                  />
+                </div>
+              </div>
+            );
+          }
+        })}
+      </section>
+    ) : (
+      <section className="no-results">
+        <div className="no-results__text">
+          <p>찾는 검색어 "{debouncedSearchTerm}"에 맞는 영화가 없습니다.</p>
+        </div>
+      </section>
+    );
+  };
+
+  return renderSearchResults();
 }
