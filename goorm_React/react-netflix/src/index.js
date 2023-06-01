600472469439ef9d8b33371cc6f1f5012006c876
summermong <2514o_o@naver.com>
2023-06-01 12:11:46 +0900

    React no.65 Deploy

--- goorm_React/react-netflix/src/index.js
+++ goorm_React/react-netflix/src/index.js
@@ -7,10 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter
-    basename="/playground-js/goorm_React
-  /react-netflix"
-  >
+  <BrowserRouter basename="/playground-js/goorm_React/react-netflix">
     <App />
   </BrowserRouter>
 );
