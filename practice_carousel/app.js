ffac171776239031c42f4f2214e795cdfeb4b01e
summermong <2514o_o@naver.com>
2023-06-28 21:49:34 +0900

    making carousel

---
+++ practice_carousel/app.js
@@ -0,0 +1,28 @@
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const carousel = document.querySelector(".carousel");
+
+let currentSlide = 1;
+const maxSlice = document.querySelectorAll(".carousel div").length;
+const imgWidth = 500;
+
+nextBtn.addEventListener("click", next);
+
+function next() {
+  if (currentSlide >= maxSlice) {
+    currentSlide = 0;
+  }
+  carousel.style.transform = `translateX(-${imgWidth * currentSlide}px)`;
+  currentSlide++;
+}
+
+prevBtn.addEventListener("click", prev);
+
+function prev() {
+  if (currentSlide === 1) {
+    currentSlide = maxSlice;
+  } else {
+    currentSlide--;
+  }
+  carousel.style.transform = `translateX(-${imgWidth * (currentSlide - 1)}px)`;
+}
