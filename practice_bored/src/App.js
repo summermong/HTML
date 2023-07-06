a281e335d91c948114b521b9b710cd10dadd10e0
summermong <2514o_o@naver.com>
2023-07-06 18:50:38 +0900

    Bored API를 이용한 리액트 토이 프로젝트

---
+++ practice_bored/src/App.js
@@ -0,0 +1,122 @@
+import { styled } from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+import boredGif from "./img/bored.gif";
+import ClickGif from "./img/1.gif";
+import ClickGif2 from "./img/2.gif";
+import ClickGif3 from "./img/3.gif";
+
+function App() {
+  const backgroundArr = [ClickGif, ClickGif2, ClickGif3];
+
+  const [background, setBackground] = useState(boredGif);
+  const [activity, setActivity] = useState("");
+  const [click, setClick] = useState(false);
+
+  const fetchActivity = async () => {
+    try {
+      const response = await axios.get("https://www.boredapi.com/api/activity");
+      setActivity(response.data.activity);
+    } catch (e) {
+      setActivity("loading...");
+    }
+  };
+
+  const handleClick = () => {
+    setClick(true);
+    fetchActivity();
+    const randomIdx = Math.floor(Math.random() * backgroundArr.length);
+    setBackground(backgroundArr[randomIdx]);
+  };
+
+  return (
+    <Wrapper>
+      <GlobalStyle />
+      <Title>
+        {click ? "How about this? 💭 " : "Are you feeling bored? 🫠"}
+      </Title>
+      <Container>
+        <Gif src={background} alt="심심한 모습" />
+        <Todo> {click ? activity : "I recommend what you do"} </Todo>
+        <Buttons>
+          <Button onClick={handleClick}> {click ? "Again" : "Click"}</Button>
+        </Buttons>
+      </Container>
+    </Wrapper>
+  );
+}
+
+const GlobalStyle = createGlobalStyle`
+body {
+  background-color: #9AC5F4;
+  margin: 0px;
+  width: 100vw;
+  height: 100vh;
+  color: white;
+  font-family: "UhBeemysen";
+}`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  height: 100vh;
+`;
+
+const Title = styled.h2`
+  width: 300px;
+  display: inline;
+  margin: 0px auto 30px;
+  text-align: center;
+  font-size: 32px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+`;
+
+const Button = styled.button`
+  border: none;
+  border-radius: 50px;
+  width: 80px;
+  height: 40px;
+  background-color: #a7ecee;
+  font-size: 26px;
+  cursor: pointer;
+  color: white;
+  margin: 10px;
+  padding-bottom: 15px;
+  font-family: "UhBeemysen";
+`;
+
+const Gif = styled.img`
+  width: 300px;
+  height: 300px;
+  display: inline;
+  margin: 0 auto;
+`;
+
+const Todo = styled.div`
+  display: inline;
+  margin: 20px auto 10px;
+  padding: 20px;
+  text-align: center;
+  font-size: 24px;
+  font-weight: bold;
+  max-width: 280px;
+  white-space: pre-line; /* 줄바꿈 처리 */
+  word-wrap: break-word; /* 단어 단위 줄바꿈 */
+`;
+
+export default App;
