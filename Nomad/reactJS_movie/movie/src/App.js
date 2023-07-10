05cd003a8497bf72e1524b79100c78271bf9d780
summermong <2514o_o@naver.com>
2023-07-10 15:08:19 +0900

    add function routes and etc

--- Nomad/reactJS_movie/movie/src/App.js
+++ Nomad/reactJS_movie/movie/src/App.js
@@ -1,48 +1,20 @@
-import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    // then보다 async
-    const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-    );
-    const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false); // 이거 안 해주면 계속 LOADING 노출
-  };
-  // 빈 배열 -> 처음 렌더 시 딱 1번만 실행
-  useEffect(() => {
-    getMovies();
-  }, []);
-
   return (
-    <div>
-      {loading ? (
-        <h1> LOADING... </h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              {/* key 설정 필요 */}
-              <h2>{movie.title}</h2>
-              <img src={movie.medium_cover_image} alt="movieImage" />
-              <p>
-                {movie.summary.length > 200
-                  ? `${movie.summary.slice(0, 201)}... `
-                  : movie.summary}
-              </p>
-              <ul>
-                {movie.genres.map((g) => (
-                  <li key={g}> {g}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          {/* Home으로 간다는 뜻 */}
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
